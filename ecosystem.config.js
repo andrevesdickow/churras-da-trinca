10843367ee8006376b1b02e1346efbc33bce46a8
andrevesdickow <andreves_dickow_7@hotmail.com>
2023-11-12 19:19:39 -0300

    feat: ajuste de script do pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,7 +25,7 @@ module.exports = {
       repo: 'git@github.com:andrevesdickow/churras-da-trinca.git',
       path: '/home/andreves/churras-da-trinca',
       'pre-deploy-local': '',
-      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install --legacy-peer-deps && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
