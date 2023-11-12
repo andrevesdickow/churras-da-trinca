d68b45975b3f14ea859272b1b228b1c1fb7fba13
andrevesdickow <andreves_dickow_7@hotmail.com>
2023-11-12 19:10:57 -0300

    feat: adicionado ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  apps : [{
+    name   : 'churras-da-trinca',
+    script : 'npm start',
+    env: {
+      NODE_ENV: 'development',
+      DATABASE_URL: 'file:./dev.db',
+      CRYPTOGRAPHY_KEY: 'MbQeThWmZq4t7w!z',
+      NEXT_PUBLIC_APP_URL: 'http://localhost:3000',
+      NEXT_TELEMETRY_DISABLED: 1
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      DATABASE_URL: 'file:./dev.db',
+      CRYPTOGRAPHY_KEY: 'MbQeThWmZq4t7w!z',
+      NEXT_PUBLIC_APP_URL: 'http://192.168.100.45:3000',
+      NEXT_TELEMETRY_DISABLED: 1
+    }
+  }],
+  deploy: {
+    production: {
+      user: 'andreves',
+      host: '192.168.100.45:3000',
+      ref: 'origin/main',
+      repo: 'git@github.com:andrevesdickow/churras-da-trinca.git',
+      path: '/home/andreves/churras-da-trinca',
+      'pre-deploy-local': '',
+      'post-deploy': 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
