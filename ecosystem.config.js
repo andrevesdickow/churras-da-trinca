741a6992f96119280b24ae0d98c9722d019b1bf2
andrevesdickow <andreves_dickow_7@hotmail.com>
2023-11-12 19:17:43 -0300

    feat: ajuste de porta

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,14 +13,14 @@ module.exports = {
       NODE_ENV: 'production',
       DATABASE_URL: 'file:./dev.db',
       CRYPTOGRAPHY_KEY: 'MbQeThWmZq4t7w!z',
-      NEXT_PUBLIC_APP_URL: 'http://192.168.100.45:3000',
+      NEXT_PUBLIC_APP_URL: 'http://192.168.100.45:4000',
       NEXT_TELEMETRY_DISABLED: 1
     }
   }],
   deploy: {
     production: {
       user: 'andreves',
-      host: '192.168.100.45:3000',
+      host: '192.168.100.45',
       ref: 'origin/main',
       repo: 'git@github.com:andrevesdickow/churras-da-trinca.git',
       path: '/home/andreves/churras-da-trinca',
